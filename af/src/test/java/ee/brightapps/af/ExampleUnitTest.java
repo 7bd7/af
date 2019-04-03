5f9519463a40e5d2cb0c462ef765369b15c15723
bd7 <77bd77@gmail.com>
2019-04-03 11:43:02 +0300

    Delete app module, add android library module

--- af/src/test/java/ee/brightapps/af/ExampleUnitTest.java
+++ af/src/test/java/ee/brightapps/af/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package ee.brightapps.af;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
