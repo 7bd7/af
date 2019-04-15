373c41748e184a00ca2ae24ed5c162bc4e4edbe2
bd7 <77bd77@gmail.com>
2019-04-15 16:26:18 +0300

    Docs for AfSerializedList added, AfGraphics separated from AfContext

--- af/src/main/java/ee/brightapps/af/AfSerializableList.java
+++ af/src/main/java/ee/brightapps/af/AfSerializableList.java
@@ -3,6 +3,10 @@ package ee.brightapps.af;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * Class allows to serialize List of Serializable objects as <b>one</b> Serializable object.
+ * @param <T> custom class, <b>should implement Serializable</b>
+ */
 public class AfSerializableList<T extends Serializable> implements Serializable {
 
     private List<T> mList;
@@ -11,6 +15,9 @@ public class AfSerializableList<T extends Serializable> implements Serializable 
         mList = list;
     }
 
+    /**
+     * @return the wrapped List of Serializable objects
+     */
     public List<T> get() {
         return mList;
     }
